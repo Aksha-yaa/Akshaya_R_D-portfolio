8e80258ee51f92416d81e7dfe9f6d980a46054d2
AKSHAYA R D <164734111+Aksha-yaa@users.noreply.github.com>
2026-03-03 23:22:44 +0530

    Import Routes and Route from react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { HashRouter } from "react-router-dom"
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-
+import { Routes, Route } from "react-router-dom";
 const queryClient = new QueryClient();
 
 const App = () => (
